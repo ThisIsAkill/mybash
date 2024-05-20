6b0afc730fcee79a549cd4cbcd0b871e9d2aac16
Akhil Moola <goudakhil29@gmail.com>
2024-05-19 22:30:56 -0600

    Add files via upload

---
+++ flatpak.sh
@@ -0,0 +1,11 @@
+# Install RPCS3 using flatpak
+flatpak install flathub org.rpcs3.RPCS3
+
+# Install Duckstation using flatpak
+flatpak install flathub io.duckstation.DuckStation
+
+# Install Motrix using flatpak
+flatpak install flathub net.agateau.Motrix
+
+# Install Plex Desktop using flatpak
+flatpak install flathub com.plex.PlexMediaPlayer
